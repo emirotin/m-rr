e40fb6cc554fe7b958f4d57a59fc6ed787b41ee2
Eugene Mirotin <emirotin@gmail.com>
2015-10-28 11:01:51 +0300

    1

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 var Sound = require('node-aplay');
 
+console.log('Start playback');
+
 // with ability to pause/resume:
-var music = new Sound('/path/to/the/file/filename.wav');
+var music = new Sound('./roll.wav');
 music.play();
 
 music.on('complete' function () {
